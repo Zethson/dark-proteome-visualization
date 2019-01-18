f83579cab2626e7b5d9d6926171306f6eb256239
Zethson <lukas.heumos@gmail.com>
2019-01-18 20:26:25 +0100

    [REFACTORING]

--- dark_proteome_visualization/handlers/copy.py
+++ dark_proteome_visualization/handlers/copy.py
@@ -19,10 +19,6 @@ LOG.setLevel(logging.INFO)
 def copy():
     dark_proteomes = parse_dark_proteome()
     dark_proteins = parse_dark_proteins()
-    LOG.info("Serializing parsed data")
-    dark_proteomes_json = json.dumps(dark_proteomes, default=lambda x: x.__dict__)
-    dark_proteins_json = json.dumps(dark_proteins, default=lambda x: x.__dict__)
-    LOG.info("Successfully serialized parsed data")
     return render_template("copy.html",
-                           dark_proteomes=dark_proteomes_json,
-                           dark_proteins=dark_proteins_json)
+                           dark_proteomes=json.dumps(dark_proteomes, default=lambda x: x.__dict__),
+                           dark_proteins=json.dumps(dark_proteins, default=lambda x: x.__dict__))
